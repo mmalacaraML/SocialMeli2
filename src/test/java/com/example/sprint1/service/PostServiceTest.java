996ed7a613019e99eda927f86e3f52af32fd970f
andr4i <andreigonzalo@outlook.com>
2024-05-07 11:27:46 -0600

    Mock e Inject Mock
    Se mockeo los repositiros para los servicios.

--- src/test/java/com/example/sprint1/service/PostServiceTest.java
+++ src/test/java/com/example/sprint1/service/PostServiceTest.java
@@ -1,8 +1,18 @@
 package com.example.sprint1.service;
 
+import com.example.sprint1.repository.PostRepositoryImpl;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
+
+    @Mock
+    PostRepositoryImpl postRepository;
+
+    @InjectMocks
+    PostServiceImpl postService;
+
 }
--- src/test/java/com/example/sprint1/service/UserServiceTest.java
+++ src/test/java/com/example/sprint1/service/UserServiceTest.java
@@ -1,8 +1,21 @@
 package com.example.sprint1.service;
 
+import com.example.sprint1.repository.UserRepositoryImpl;
+import com.example.sprint1.repository.UserRepositoryTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    @Mock
+    UserRepositoryImpl userRepository;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
 }
